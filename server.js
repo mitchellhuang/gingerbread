20c3af26ea85206cf5c04c08899d5185b4d601b9
Mitchell Huang <mitchell@mitchellhuang.net>
2018-08-01 14:31:55 -0700

    Serverless

--- server.js
+++ server.js
@@ -6,14 +6,24 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const server = express();
-server.get('*', (req, res) => handle(req, res));
+const createServer = () => {
+  const server = express();
+  server.get('*', (req, res) => handle(req, res));
+  return server;
+};
 
-app.prepare()
-  .then(() => {
-    server.listen(port, (err) => {
-      if (err) throw err;
-      // eslint-disable-next-line
-      console.log(`> Ready on http://localhost:${port}`);
+const server = createServer();
+
+if (!process.env.LAMBDA) {
+  app.prepare()
+    .then(() => {
+      server.listen(port, (err) => {
+        if (err) throw err;
+        // eslint-disable-next-line
+        console.log(`> Ready on http://localhost:${port}`);
+      });
     });
-  });
+}
+
+exports.app = app;
+exports.server = server;
